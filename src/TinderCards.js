1cc5159a74c943ee2f720f6f2670e62cf00d0b31
CannedShroud <hypersync112@gmail.com>
2020-09-15 21:54:01 +0530

    Init TinderCards Component

---
+++ src/TinderCards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TinderCards.css";
+
+function TinderCards() {
+  return (
+    <div className="tinderCards">
+      <h2>ME is TinderCards</h2>
+    </div>
+  );
+}
+
+export default TinderCards;
