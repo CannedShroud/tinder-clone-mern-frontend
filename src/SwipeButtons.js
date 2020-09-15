3540491bad7e2cdd7d1a1722a9e2fa893c408b89
CannedShroud <hypersync112@gmail.com>
2020-09-15 23:12:46 +0530

    Build SwipeButtons Component

--- src/SwipeButtons.js
+++ src/SwipeButtons.js
@@ -1,10 +1,26 @@
+import { Close, Favorite, FlashOn, Replay, StarRate } from "@material-ui/icons";
+import { IconButton } from "@material-ui/core";
 import React from "react";
 import "./SwipeButtons.css";
 
 function SwipeButtons() {
   return (
     <div className="swipeButtons">
-      <h3>I am SwipeButtons</h3>
+      <IconButton className="swipeButtons__repeat">
+        <Replay fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__left">
+        <Close fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__star">
+        <StarRate fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__right">
+        <Favorite fontSize="large" />
+      </IconButton>
+      <IconButton className="swipeButtons__lightening">
+        <FlashOn fontSize="large" />
+      </IconButton>
     </div>
   );
 }
