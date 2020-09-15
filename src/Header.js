4d5ac4dccac211ee4e69998b8ab78580fb42a59f
CannedShroud <hypersync112@gmail.com>
2020-09-15 21:51:14 +0530

    Build Header Component

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,22 @@
+import { IconButton } from "@material-ui/core";
+import { Forum, Person } from "@material-ui/icons";
 import React from "react";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <h1>Header me is</h1>
+      <IconButton>
+        <Person fontSize="large" className="header__icon" />
+      </IconButton>
+      <img
+        src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png"
+        alt="logo"
+        className="header__logo"
+      />
+      <IconButton fontSize="large" className="header__icon">
+        <Forum />
+      </IconButton>
     </div>
   );
 }
