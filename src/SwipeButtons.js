2704de70bac7141034118b0f69d83aa2bfb46238
CannedShroud <hypersync112@gmail.com>
2020-09-15 22:55:43 +0530

    Init SwipeButtons Component

---
+++ src/SwipeButtons.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./SwipeButtons.css";
+
+function SwipeButtons() {
+  return (
+    <div className="swipeButtons">
+      <h3>I am SwipeButtons</h3>
+    </div>
+  );
+}
+
+export default SwipeButtons;
